937e2b9770a4bef911a3521663f18c9184e37231
Hiren <Hiren@pcit92>
2026-09-08 12:03:23 +0530

    Use relative path so scripts work on any PC

--- Pull_Save.bat
+++ Pull_Save.bat
@@ -3,12 +3,7 @@ echo Pulling Minecraft save from GitHub...
 echo.
 
 set REPO_URL=https://github.com/tadhanihiren/Minecraft_BMC_1.16_Save.git
-set SAVE_DIR=F:\Saves\golida
-
-if exist "%SAVE_DIR%" (
-    echo Save folder already exists. Backing up...
-    ren "%SAVE_DIR%" "golida_backup_%date:~-4%%date:~4,2%%date:~7,2%"
-)
+set SAVE_DIR=%~dp0golida
 
 echo Cloning repository...
 git clone %REPO_URL% "%TEMP%\Minecraft_BMC_Save"
@@ -19,7 +14,8 @@ if %errorlevel% neq 0 (
     exit /b 1
 )
 
-echo Copying to saves...
+echo Copying save to...
+echo   %SAVE_DIR%
 mkdir "%SAVE_DIR%"
 xcopy /E /I /Y "%TEMP%\Minecraft_BMC_Save" "%SAVE_DIR%"
 
--- Push_Save.bat
+++ Push_Save.bat
@@ -3,10 +3,10 @@ echo Pushing Minecraft save to GitHub...
 echo.
 
 set REPO_URL=https://github.com/tadhanihiren/Minecraft_BMC_1.16_Save.git
-set SAVE_DIR=F:\Saves\golida
+set SAVE_DIR=%~dp0golida
 
 if not exist "%SAVE_DIR%" (
-    echo Error: Minecraft save folder not found!
+    echo Error: Save folder not found at %SAVE_DIR%
     pause
     exit /b 1
 )
